76be531e5ccd8ea4af948c8283ff647247a092d6
Luciano Matsuo <matsuoluciano@gmail.com>
2016-08-29 18:38:49 -0300

    ..

---
+++ js/canvas.js
@@ -0,0 +1,6 @@
+var canvas = document.getElementById('canvas');
+var contexto = canvas.getContext("2d");
+contexto.fillStyle="blue";//regla de rellenado color azul
+contexto.moveTo(0,0);
+contexto.lineTo(300,300);
+contexto.stroke();
